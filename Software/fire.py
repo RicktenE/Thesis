675a5c7c9553b40d9224002b4e0f5afe47867cca
RicktenE <60656316+RicktenE@users.noreply.github.com>
2023-05-08 11:02:07 +0200

    Settings for forest fire model for presentation

--- Software/fire.py
+++ Software/fire.py
@@ -17,6 +17,7 @@ class Fire(DynamicModel):
 
     self.dem = self.readmap('dem')
     self.gradient = slope(self.dem)
+    self.report(self.gradient, 'gradient')
 
     #get the number of pixels for the machine learning model
     global number_pixels
@@ -30,43 +31,46 @@ class Fire(DynamicModel):
 
     potentialNewFire = ifthenelse(self.neighbourBurns & ~boolean(self.fire) , boolean(1), boolean(0))
 
-    # fire_elevation = self.dem * self.fire
-    # PNF_elevation = scalar(potentialNewFire) * scalar(self.dem)
-    #
-    # # function part
-    # def elevation_probability(firemap, diagonal, shift, fire_elevation, PNF_elevation):
-    #   import math as m
-    #   F_neighbor_elevation = ifthenelse(firemap == 1, 0, shift0(fire_elevation, shift[0], shift[1]))
-    #
-    #   if diagonal == True:
-    #     angle = atan(((scalar(boolean(F_neighbor_elevation)) * PNF_elevation) - F_neighbor_elevation)*(-1) / m.sqrt(2))
-    #   else:
-    #     angle = atan(((scalar(boolean(F_neighbor_elevation)) * PNF_elevation) - F_neighbor_elevation)*(-1))
-    #
-    #   angle_trans = ifthenelse(scalar(angle) > 180, scalar(angle) - 360, scalar(angle))
-    #   prob_elev = m.e ** (0.1 * scalar(angle_trans)) * scalar(boolean(F_neighbor_elevation))
-    #   return prob_elev
-    #
-    # p_NW = elevation_probability(self.fire,  True, [1, 1] , fire_elevation, PNF_elevation)
-    # p_NE = elevation_probability(self.fire,  True, [1, -1], fire_elevation, PNF_elevation)
-    # p_SE = elevation_probability(self.fire,  True, [-1, -1], fire_elevation, PNF_elevation)
-    # p_SW = elevation_probability(self.fire,  True, [-1, 1], fire_elevation, PNF_elevation)
-    # p_N = elevation_probability(self.fire,  False, [1, 0], fire_elevation, PNF_elevation)
-    # p_E = elevation_probability(self.fire,  False, [0, -1], fire_elevation, PNF_elevation)
-    # p_S = elevation_probability(self.fire,  False, [-1, 0], fire_elevation, PNF_elevation)
-    # p_W = elevation_probability(self.fire,  False, [0, 1], fire_elevation, PNF_elevation)
-    #
-    # pElevation = p_N + p_S + p_W + p_E + p_NW + p_NE + p_SW + p_SE
-    #
-    #
-    # p = 0.1  * pElevation
-    p = 0.25
+    fire_elevation = self.dem * self.fire
+    PNF_elevation = scalar(potentialNewFire) * scalar(self.dem)
+
+    # function part
+    def elevation_probability(firemap, diagonal, shift, fire_elevation, PNF_elevation):
+      import math as m
+      F_neighbor_elevation = ifthenelse(firemap == 1, 0, shift0(fire_elevation, shift[0], shift[1]))
+
+      if diagonal == True:
+        angle = atan(((scalar(boolean(F_neighbor_elevation)) * PNF_elevation) - F_neighbor_elevation)*(-1) / m.sqrt(2))
+      else:
+        angle = atan(((scalar(boolean(F_neighbor_elevation)) * PNF_elevation) - F_neighbor_elevation)*(-1))
+
+      angle_trans = ifthenelse(scalar(angle) > 180, scalar(angle) - 360, scalar(angle))
+      prob_elev = 0.001*m.e ** (0.05 * scalar(angle_trans)) * scalar(boolean(F_neighbor_elevation))
+      return prob_elev
+
+    p_NW = elevation_probability(self.fire,  True, [1, 1] , fire_elevation, PNF_elevation)
+    p_NE = elevation_probability(self.fire,  True, [1, -1], fire_elevation, PNF_elevation)
+    p_SE = elevation_probability(self.fire,  True, [-1, -1], fire_elevation, PNF_elevation)
+    p_SW = elevation_probability(self.fire,  True, [-1, 1], fire_elevation, PNF_elevation)
+    p_N = elevation_probability(self.fire,  False, [1, 0], fire_elevation, PNF_elevation)
+    p_E = elevation_probability(self.fire,  False, [0, -1], fire_elevation, PNF_elevation)
+    p_S = elevation_probability(self.fire,  False, [-1, 0], fire_elevation, PNF_elevation)
+    p_W = elevation_probability(self.fire,  False, [0, 1], fire_elevation, PNF_elevation)
+
+    pElevation = p_N + p_S + p_W + p_E + p_NW + p_NE + p_SW + p_SE
+
+
+    # p = 0.05  + pElevation
+    p = self.gradient + pElevation
+    # p = 0.25
     realization = scalar(uniform(1) <= p)
 
     NewFire = ifthenelse(boolean(potentialNewFire) & boolean(realization), boolean(1), boolean(0))
     # self.report(NewFire, 'FireEdge')
 
-    # writing for ml application. Save entire map each timestep as a long flat array
+    # writing for ml application. Save entire map each timestep as a long flat array.
+    # Where there are NaN's replace with -1 for visual representation in ML classification of 1's and 0's. if Nan's set to -9999
+    # The rest of the values are not visible anymore
     fire_array = pcr2numpy(self.fire, -1)
     flat_fire = fire_array.flatten()
     global array_for_ml
@@ -84,7 +88,7 @@ class Fire(DynamicModel):
 
 
 
-nrOfTimeSteps=80
+nrOfTimeSteps=100
 myModel = Fire()
 dynamicModel = DynamicFramework(myModel,nrOfTimeSteps)
 dynamicModel.run()
