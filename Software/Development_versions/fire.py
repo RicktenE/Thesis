a7307565cdff56d2d9f9615e4d55439c1799f043
RicktenE <60656316+RicktenE@users.noreply.github.com>
2023-06-13 11:33:51 +0200

    Reorganisation of Repo

---
+++ Software/Development_versions/fire.py
@@ -0,0 +1,118 @@
+from pcraster import *
+from pcraster.framework import *
+import numpy as np
+
+#Initialise variables for later use
+fire_array = np.empty(0)
+# precipitation_array = np.empty(0)
+dem_array = np.empty(0)
+# temp_array = np.empty(0)
+# snowfall_array = np.empty(0)
+# rainfall_array = np.empty(0)
+# actualmelt_array = np.empty(0)
+# runoff_array = np.empty(0)
+horizontal_pixels = 0
+vertical_pixels = 0
+timesteps = 0
+
+class Fire(DynamicModel):
+  def __init__(self):
+    DynamicModel.__init__(self)
+    setclone('clone.map')
+
+  def initial(self):
+    # ----------------------------------------------------------------------------#
+    # Define initial conditions
+    # ----------------------------------------------------------------------------#
+
+    #set initial fire map and change to scalar for later transformations
+    self.fire = readmap('start.map')
+    self.fire = scalar(self.fire)
+
+    self.dem = self.readmap('firedem')
+    self.gradient = slope(self.dem)
+    self.report(self.gradient, 'gradient')
+
+    # ----------------------------------------------------------------------------#
+    # Prepare static data for ML read-out
+    # ----------------------------------------------------------------------------#
+    global horizontal_pixels
+    global vertical_pixels
+    global dem_array
+
+    horizontal_pixels = len(pcr_as_numpy(self.dem)[1, :])
+    vertical_pixels = len(pcr_as_numpy(self.dem)[:, 1])
+    demmap_array = pcr2numpy(self.dem, -1)
+    demmap_array_flat = demmap_array.flatten()
+    dem_array = np.append(dem_array, demmap_array_flat)
+  def dynamic(self):
+
+    neighbours = window4total(self.fire)
+    self.neighbourBurns = ifthenelse(neighbours > 0, boolean(1), boolean(0))
+    # self.report(self.neighbourBurns, 'neighB')
+
+    potentialNewFire = ifthenelse(self.neighbourBurns & ~boolean(self.fire) , boolean(1), boolean(0))
+
+    fire_elevation = self.dem * self.fire
+    PNF_elevation = scalar(potentialNewFire) * scalar(self.dem)
+
+    # function part
+    def elevation_probability(firemap, diagonal, shift, fire_elevation, PNF_elevation):
+      import math as m
+      F_neighbor_elevation = ifthenelse(firemap == 1, 0, shift0(fire_elevation, shift[0], shift[1]))
+
+      if diagonal == True:
+        angle = atan(((scalar(boolean(F_neighbor_elevation)) * PNF_elevation) - F_neighbor_elevation)*(-1) / m.sqrt(2))
+      else:
+        angle = atan(((scalar(boolean(F_neighbor_elevation)) * PNF_elevation) - F_neighbor_elevation)*(-1))
+
+      angle_trans = ifthenelse(scalar(angle) > 180, scalar(angle) - 360, scalar(angle))
+      prob_elev = 0.001*m.e ** (0.05 * scalar(angle_trans)) * scalar(boolean(F_neighbor_elevation))
+      return prob_elev
+
+    p_NW = elevation_probability(self.fire,  True, [1, 1] , fire_elevation, PNF_elevation)
+    p_NE = elevation_probability(self.fire,  True, [1, -1], fire_elevation, PNF_elevation)
+    p_SE = elevation_probability(self.fire,  True, [-1, -1], fire_elevation, PNF_elevation)
+    p_SW = elevation_probability(self.fire,  True, [-1, 1], fire_elevation, PNF_elevation)
+    p_N = elevation_probability(self.fire,  False, [1, 0], fire_elevation, PNF_elevation)
+    p_E = elevation_probability(self.fire,  False, [0, -1], fire_elevation, PNF_elevation)
+    p_S = elevation_probability(self.fire,  False, [-1, 0], fire_elevation, PNF_elevation)
+    p_W = elevation_probability(self.fire,  False, [0, 1], fire_elevation, PNF_elevation)
+
+    pElevation = p_N + p_S + p_W + p_E + p_NW + p_NE + p_SW + p_SE
+
+
+    # p = 0.05  + pElevation
+    p = self.gradient + pElevation
+    # p = 0.25
+    realization = scalar(uniform(1) <= p)
+
+    NewFire = ifthenelse(boolean(potentialNewFire) & boolean(realization), boolean(1), boolean(0))
+    # self.report(NewFire, 'FireEdge')
+
+    # writing for ml application. Save entire map each timestep as a long flat array.
+    # Where there are NaN's replace with -1 for visual representation in ML classification of 1's and 0's. if Nan's set to -9999
+    # The rest of the values are not visible anymore
+    firemap_array = pcr2numpy(self.fire, -1)
+    firemap_array_flat = firemap_array.flatten()
+    global fire_array
+    fire_array = np.append(fire_array, firemap_array_flat)
+
+    #just a check to see if it works properly
+    # print('        ')
+    # print(array_for_ml)
+
+
+    #update fire status
+    self.fire = (self.fire + scalar(NewFire))
+    # print('check')
+    self.report(self.fire, 'fire')
+
+
+
+nrOfTimeSteps=100
+timesteps = nrOfTimeSteps
+myModel = Fire()
+dynamicModel = DynamicFramework(myModel,nrOfTimeSteps)
+dynamicModel.run()
+
---
+++ Software/ML-emulations/Snowmeltmodel_variable.py
@@ -0,0 +1,150 @@
+from pcraster.framework import *
+import numpy as np
+
+#Initialise variables for later use
+snow_array = np.empty(0)
+precipitation_array = np.empty(0)
+dem_array = np.empty(0)
+temp_array = np.empty(0)
+snowfall_array = np.empty(0)
+rainfall_array = np.empty(0)
+actualmelt_array = np.empty(0)
+runoff_array = np.empty(0)
+horizontal_pixels = 0
+vertical_pixels = 0
+timesteps = 0
+
+class MyFirstModel(DynamicModel):
+    def __init__(self):
+        DynamicModel.__init__(self)
+        setclone('dem.map')
+
+    def initial(self):
+        # ----------------------------------------------------------------------------#
+        # Define initial conditions
+        # ----------------------------------------------------------------------------#
+        dem = self.readmap('dem')
+        self.snow = 0.0
+        elevationMeteoStation = 2058.1
+        elevationAboveMeteoStation = dem - elevationMeteoStation
+
+        global rate
+        temperatureLapseRate = rate
+        print(' \n Temperature lapse rate is: ', temperatureLapseRate, '\n')
+        self.temperatureCorrection = elevationAboveMeteoStation * temperatureLapseRate
+
+        # ----------------------------------------------------------------------------#
+        # Prepare static data for ML read-out
+        # ----------------------------------------------------------------------------#
+        global horizontal_pixels
+        global vertical_pixels
+        global dem_array
+
+        horizontal_pixels = len(pcr_as_numpy(dem)[1,:])
+        vertical_pixels = len(pcr_as_numpy(dem)[:,1])
+        demmap_array = pcr2numpy(dem, -1)
+        demmap_array_flat = demmap_array.flatten()
+        dem_array = np.append(dem_array, demmap_array_flat)
+        # ----------------------------------------------------------------------------#
+
+    def dynamic(self):
+        # ----------------------------------------------------------------------------#
+        # Calculate dynamic variables
+        # ----------------------------------------------------------------------------#
+        # Precipitation
+        precipitation = timeinputscalar('precip.tss', 1)
+
+        # Temperature
+        temperatureObserved = timeinputscalar('temp.tss', 1)
+        temperature = temperatureObserved - self.temperatureCorrection
+
+        # Rainfall / Snowfall
+        freezing = temperature < 0.0
+        snowFall = ifthenelse(freezing, precipitation, 0.0)
+        rainFall = ifthenelse(pcrnot(freezing), precipitation, 0.0)
+
+        # Calculate snow -- Add fallen snow subtract melted
+        self.snow = self.snow + snowFall
+        potentialMelt = ifthenelse(pcrnot(freezing), temperature * 0.01, 0)
+        actualMelt = min(self.snow, potentialMelt)
+        self.snow = self.snow - actualMelt
+
+        # Calculate generated Runoff
+        runoffGenerated = actualMelt + rainFall
+
+        # ----------------------------------------------------------------------------#
+        # Report dynamic variables to maps
+        # ----------------------------------------------------------------------------#
+        # self.report(temperatureObserved, 'tempObs')
+        # self.report(temperature, 'temp')
+        # self.report(freezing, 'fr')
+        # self.report(snowFall, 'snF')
+        # self.report(rainFall, 'rF')
+        # self.report(potentialMelt, 'pmelt')
+        # self.report(actualMelt, 'amelt')
+        # self.report(self.snow, 'snow')
+        # self.report(runoffGenerated, 'rg')
+
+        # ----------------------------------------------------------------------------#
+        # Prepare dynamic drivers for ML read-out
+        # ----------------------------------------------------------------------------#
+        # Precipitation
+        precipmap_array = pcr2numpy(precipitation, -1)
+        precipmap_array_flat = precipmap_array.flatten()
+        global precipitation_array
+        precipitation_array = np.append(precipitation_array, precipmap_array_flat)
+
+        # Temperature
+        tempmap_array = pcr2numpy(temperature, -1)
+        tempmap_array_flat = tempmap_array.flatten()
+        global temp_array
+        temp_array = np.append(temp_array, tempmap_array_flat)
+
+        # Rainfall
+        rnfllmap_array = pcr2numpy(rainFall, -1)
+        rnfllmap_array_flat = rnfllmap_array.flatten()
+        global rainfall_array
+        rainfall_array = np.append(rainfall_array, rnfllmap_array_flat)
+
+        # Snowfall
+        snwfllmap_array = pcr2numpy(snowFall, -1)
+        snwfllmap_array_flat = snwfllmap_array.flatten()
+        global snowfall_array
+        snowfall_array = np.append(snowfall_array, snwfllmap_array_flat)
+
+        # Actual melt
+        ameltmap_array = pcr2numpy(actualMelt, -1)
+        ameltmap_array_flat = ameltmap_array.flatten()
+        global actualmelt_array
+        actualmelt_array = np.append(actualmelt_array, ameltmap_array_flat)
+
+        # Run off
+        runoffmap_array = pcr2numpy(runoffGenerated, -1)
+        runoffmap_array_flat = runoffmap_array.flatten()
+        global runoff_array
+        runoff_array = np.append(runoff_array, runoffmap_array_flat)
+
+        # Snow
+        snowmap_array = pcr2numpy(self.snow, -1)
+        snowmap_array_flat = snowmap_array.flatten()
+        global snow_array
+        snow_array = np.append(snow_array, snowmap_array_flat)
+        # ----------------------------------------------------------------------------#
+
+
+nrOfTimeSteps = 181
+timesteps = nrOfTimeSteps
+
+
+temprate = [0.002, 0.003, 0.004, 0.005, 0.001]
+# temprate = [0.001, 0.003, 0.004, 0.005, 0.002]
+# temprate = [0.001, 0.002, 0.004, 0.005, 0.003]
+# temprate = [0.001, 0.002, 0.003, 0.005, 0.004]
+# temprate = [0.001, 0.002, 0.003, 0.004, 0.005]
+variable_list = temprate
+
+for iii in range(len(variable_list)):
+    rate = temprate[iii]
+    myModel = MyFirstModel()
+    dynamicModel = DynamicFramework(myModel, nrOfTimeSteps)
+    dynamicModel.run()
